fc371f806bbdcd1fbbcb125445d54adff8233e96
mikyrenato <mikyrenato@yahoo.com>
2023-04-01 19:13:15 +0000

    Fix deployment problems

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [BASE_DIR / 'assets']
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
