d6bb12e1c8c83c5f57788b329fa15effb750845a
mikyrenato <mikyrenato@yahoo.com>
2023-04-02 09:41:15 +0000

    Deployment

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
