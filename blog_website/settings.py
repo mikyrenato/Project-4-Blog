d3deaea9299023ade4be71cf0858b34c7a961208
mikyrenato <mikyrenato@yahoo.com>
2023-04-02 10:59:16 +0000

    Deployment

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['project-4-blog.herokuapp.com/', 'localhost', '8080-mikyrenato-project4blog-6rgfqrfwlqy.ws-eu93.gitpod.io']
+ALLOWED_HOSTS = ['project-4-blog.herokuapp.com', 'localhost', '8080-mikyrenato-project4blog-6rgfqrfwlqy.ws-eu93.gitpod.io']
 
 
 # Application definition
